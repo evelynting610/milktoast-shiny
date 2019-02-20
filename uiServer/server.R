4530c6c5e3f64fd265a811e279f626be057152c0
evelynting610 <esting610@gmail.com>
2019-02-20 10:13:00 -0600

    testing on separate ui and server

--- uiServer/server.R
+++ uiServer/server.R
@@ -1,5 +1,8 @@
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  print("server has started")
   output$test <- renderText({
     paste("In the separate server folder")
   })
+  observe(input$alive_count)
+  session$allowReconnect("force")
 })
--- uiServer/ui.R
+++ uiServer/ui.R
@@ -1,5 +1,7 @@
 shinyUI(
   fluidPage(
+    tags$head(HTML("
+                <script type='text/javascript' src='keep-alive.js'></script>")),
     titlePanel("Separate UI title"),
     mainPanel(
       textOutput("test")
